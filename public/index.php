287728268620e08ef85fbe6c365790a1028c35f9
dadehnamair <dadehnamair@gmail.com>
2026-09-02 19:29:44 +0330

    feat: update public path configuration for shared hosting support

--- public/index.php
+++ public/index.php
@@ -17,4 +17,7 @@ require __DIR__.'/../vendor/autoload.php';
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// Shared hosting: public assets are served from this directory, not include/public
+$app->usePublicPath(__DIR__);
+
 $app->handleRequest(Request::capture());
